33a339113ef2997a3f00eafbfce77aaeb9a19cbf
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-10-16 07:14:22 +0700

    12-14

---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const PlaceDetailScreen = (props) => {
+  return (
+    <View>
+      <Text>PlaceDetailScreen</Text>
+    </View>
+  );
+};
+
+PlaceDetailScreen.navigationOptions = (navData) => ({
+  headerTitle: navData.navigation.getParam('placeTitle'),
+});
+
+const styles = StyleSheet.create({});
+
+export default PlaceDetailScreen;
---
+++ store/places-actions.js
@@ -0,0 +1,33 @@
+import * as FileSystem from 'expo-file-system';
+
+export const ADD_PLACE = 'ADD_PLACE';
+import { insertPlace } from '../helpers/db';
+
+export const addPlace = (title, image) => {
+  return async (dispatch) => {
+    const fileName = image.split('/').pop();
+    const newPath = FileSystem.documentDirectory;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        'Dummy Address',
+        15.6,
+        12.3
+      );
+      console.log(dbResult);
+      dispatch({
+        type: ADD_PLACE,
+        placeData: { id: dbResult.insertId, title: title, image: newPath },
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+};
